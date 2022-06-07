1db03eb86e99d9980327e02371068a52958f4a10
Vesa Hajdari <76015996+vesahajdari@users.noreply.github.com>
2022-06-07 23:40:26 +0200

    Update load.js

--- P1/load.js
+++ P1/load.js
@@ -66,3 +66,163 @@ class GIF extends GameObject{
         this.cooldown = performance.now()
         this.degrees = 0
     }
+    
+     draw(){
+        if (this.amt_of_lists != 1){
+            if (this.degrees == 0){
+                this.image_being_drawn = this.image_list[0][this.index]
+            }
+            else if (this.degrees == 90){
+                this.image_being_drawn = this.image_list[1][this.index]
+            }
+            else if (this.degrees == 270){
+                this.image_being_drawn = this.image_list[2][this.index]
+            }
+            else if (this.degrees == 180){
+                this.image_being_drawn = this.image_list[3][this.index]
+            }
+        }
+        else {
+            this.image_being_drawn = this.image_list[this.index]
+        }
+        ctx.drawImage(this.image_being_drawn, this.x - this.image_being_drawn.width/2, this.y - this.image_being_drawn.height/2, this.image_being_drawn.width, this.image_being_drawn.height)
+        if (performance.now() - this.cooldown >= 200){
+            if (this.amt_of_lists == 1){
+                if (this.image_list.length - 1 == this.index){
+                    this.index = 0
+                }
+                else {
+                    this.index++
+                }
+                this.cooldown = performance.now()
+            }
+            else {
+                if (this.image_list.length/this.amt_of_lists == this.index){
+                    this.index = 0
+                }
+                else {
+                    this.index++
+                }
+                this.cooldown = performance.now()
+            }
+            
+        }   
+        
+    }
+    add(image){
+        this.image_list.push(image)
+    }
+}
+
+for (let i = starting_position; i <= starting_position + 900; i = i + 5){ //map generation
+    if (i == starting_position + 200){
+        for (let j = starting_y_position + 5; j < starting_y_position + 400; j = j + 5){
+            eval('var node' + i + j + "= new node(" + i + "," + j + ")")
+            eval('nodes.add(' + "node" + i + j + ')')
+        }
+    }
+    else if (i == starting_position + 250){
+        for (let j = starting_y_position + 545; j > starting_y_position + 400; j = j - 5){
+            eval('var node' + i + j + "= new node(" + i + "," + j + ")")
+            eval('nodes.add(' + "node" + i + j + ')')
+        }
+    }
+    else if (i == starting_position){
+        for (let j = starting_y_position; j <= starting_y_position + 150; j = j + 5){
+            eval('var node' + i + j + "= new node(" + i + "," + j + ")")
+            eval('nodes.add(' + "node" + i + j + ')')
+        }
+        for (let j = starting_y_position + 550; j > starting_y_position + 400; j = j - 5){
+            eval('var node' + i + j + "= new node(" + i + "," + j + ")")
+            eval('nodes.add(' + "node" + i + j + ')')
+        }
+    }
+    else if (i == starting_position + 375){
+        for (let j = starting_y_position + 5; j < starting_y_position + 400; j = j + 5){
+            eval('var node' + i + j + "= new node(" + i + "," + j + ")")
+            eval('nodes.add(' + "node" + i + j + ')')
+        }
+    }
+    else if (i == starting_position + 285){
+        for (let j = starting_y_position + 185; j < starting_y_position + 270; j = j + 5){
+            eval('var node' + i + j + "= new node(" + i + "," + j + ")")
+            eval('nodes.add(' + "node" + i + j + ')')
+        }
+    }
+    if (i <= starting_position + 323 && i >= starting_position + 285){
+        let j = starting_y_position + 270
+        eval('var node' + i + j + "= new node(" + i + "," + j + ")")
+        eval('nodes.add(' + "node" + i + j + ')')
+    }
+    if (i < starting_position + 375 && i >= starting_position + 285){
+        let j = starting_y_position + 180
+        eval('var node' + i + j + "= new node(" + i + "," + j + ")")
+        eval('nodes.add(' + "node" + i + j + ')')
+    }
+    if (i < starting_position + 550){
+        let j = starting_y_position + 400
+        eval('var node' + i + j + "= new node(" + i + "," + j + ")")
+        eval('nodes.add(' + "node" + i + j + ')')
+    }
+    else if (i == starting_position + 550){
+        for (let j = starting_y_position + 400; j < starting_y_position + 475; j = j + 5){
+            eval('var node' + i + j + "= new node(" + i + "," + j + ")")
+            eval('nodes.add(' + "node" + i + j + ')')
+        }
+    }
+    if (i > starting_position + 250 && i < starting_position + 725){
+        let j = starting_y_position + 475
+        eval('var node' + i + j + "= new node(" + i + "," + j + ")")
+        eval('nodes.add(' + "node" + i + j + ')')
+    }
+    else if (i == starting_position + 725){
+        for (let j = starting_y_position + 475; j > starting_y_position; j = j - 5){
+            if (j != starting_y_position + 200 && j != starting_y_position + 325) {
+                eval('var node' + i + j + "= new node(" + i + "," + j + ")")
+                eval('nodes.add(' + "node" + i + j + ')')
+            }
+        }
+    }
+    if (i > starting_position + 375 && i < starting_position + 565){
+        let j = starting_y_position + 100
+        eval('var node' + i + j + "= new node(" + i + "," + j + ")")
+        eval('nodes.add(' + "node" + i + j + ')')
+    }
+    else if (i == starting_position + 565){
+        for (let j = starting_y_position + 100; j <= starting_y_position + 200; j = j + 5){
+            eval('var node' + i + j + "= new node(" + i + "," + j + ")")
+            eval('nodes.add(' + "node" + i + j + ')')
+        }
+    }
+    else if (i > starting_position + 565 && i < starting_position + 900){
+        let j = starting_y_position + 200
+        eval('var node' + i + j + "= new node(" + i + "," + j + ")")
+        eval('nodes.add(' + "node" + i + j + ')')
+    }
+    if (i <= starting_position + 195){
+        let j = starting_y_position + 155
+        eval('var node' + i + j + "= new node(" + i + "," + j + ")")
+        eval('nodes.add(' + "node" + i + j + ')')
+        j = starting_y_position + 280
+        eval('var node' + i + j + "= new node(" + i + "," + j + ")")
+        eval('nodes.add(' + "node" + i + j + ')')
+    }
+    if (i > starting_position + 375 && i < starting_position + 900){
+        let j = starting_y_position + 325
+        eval('var node' + i + j + "= new node(" + i + "," + j + ")")
+        eval('nodes.add(' + "node" + i + j + ')')
+    }
+    if (i == starting_position + 900){
+        for (let j = starting_y_position; j <= starting_y_position + 550; j = j + 5){
+            eval('var node' + i + j + "= new node(" + i + "," + j + ")")
+            eval('nodes.add(' + "node" + i + j + ')')
+        }
+    }
+    if (i >= starting_position + 5 && i <= starting_position + 895) {
+        eval('var node' + i + starting_y_position + "= new node(" + i + "," + starting_y_position + ")")
+        eval('nodes.add(' + "node" + i + starting_y_position + ')')
+        eval('var node' + i + (starting_y_position + 550) + "= new node(" + i + "," + (starting_y_position + 550) + ")")
+        eval('nodes.add(' + "node" + i + (starting_y_position + 550) + ')')
+    }
+}
+
