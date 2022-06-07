2dae72435ad0b628ce6bdfd919a70cda9024c115
Edi Morina <74329392+EdiMorina1@users.noreply.github.com>
2022-06-07 23:37:46 +0200

    Update load.js

--- P1/load.js
+++ P1/load.js
@@ -1,1 +1,68 @@
 //init
+var ghost_list = []
+var starting_position = 300
+var starting_y_position = 100
+var walls = []
+var nodes = new Set()
+
+class node {
+    constructor(x, y, connecting_nodes = []){
+        this.x = x
+        this.y = y
+        this.connecting_nodes = connecting_nodes
+    }
+}
+
+class wall {
+    constructor(x, y, width, height){
+        this.x = x
+        this.y = y
+        this.width = width
+        this.height = height
+    }
+    draw() {
+        ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
+}
+
+class GameObject {
+    constructor(x, y, image){
+        this.x = x
+        this.y = y
+        this.image = new Image()
+        this.image.src = image
+    }
+    draw(width = this.image.width, height = this.image.height){
+        ctx.drawImage(this.image, this.x - width/2, this.y - height/2, width, height)
+    }
+}
+
+class GIF extends GameObject{
+    constructor(x, y, list, amt_of_lists = 1){
+        super(x, y, null)
+        this.original_location_x = x
+        this.original_location_y = y
+        this.image_list = list
+        if (amt_of_lists == 1){
+            for (let i = 0; i < list.length; i++){
+                var image = new Image()
+                image.src = list[i]
+                list[i] = image
+            }
+        }
+        else {
+            for (let i = 0; i < list.length; i++){
+                for (let j = 0; j < list.length; j++){
+                    if (list[i][j] != undefined){
+                        var image = new Image()
+                        image.src = list[i][j]
+                        list[i][j] = image
+                    }
+                }
+            }
+        }
+        this.amt_of_lists = amt_of_lists
+        this.index = 0
+        this.cooldown = performance.now()
+        this.degrees = 0
+    }
